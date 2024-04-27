ea8b15831f33cd6671b8edc5105f4beabd331d26
CallMeKennn <huyhoang13199@gmail.com>
2024-04-27 13:52:12 +0700

    QRcodeReview Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import { InputGroup, Form, Button } from "react-bootstrap";
 import Style from "./Component/Style";
 import Frame from "./Component/Frame";
 import Logo from "./Component/Logo";
+import QRcodeReview from "./Component/QRcodeReview";
+
 import { faGrip } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -14,44 +16,65 @@ function App() {
     const [isStyleComponent, setIsStyleComponent] = useState(false);
     const [isFrameComponent, setIsFrameComponent] = useState(false);
     const [isLogoComponent, setIsLogoComponent] = useState(false);
+    const [isUrlValid, setIsUrlValid] = useState(true)
+    const [url, setUrl] = useState('')
+
+    const handleValidUrlField = (checkValid) => {
+        setIsUrlValid(checkValid)
+    }
+
+    const handleMountStyleComponent = () => {
+        setIsStyleComponent(true);
+        setIsFrameComponent(false);
+        setIsLogoComponent(false);
+    };
+
+    const handleMountLogoComponent = () => {
+        setIsStyleComponent(false);
+        setIsFrameComponent(false);
+        setIsLogoComponent(true);
+    };
+
+    const handleMountFrameComponent = () => {
+        setIsStyleComponent(false);
+        setIsFrameComponent(true);
+        setIsLogoComponent(false);
+    };
 
     return (
         <div className="App d-flex justify-content-between">
-            <div className="bg-success-subtle col-8">
-                <div>
-                    <h1>QR Code Generator</h1>
-                    <InputGroup className="mb-3">
+            <div className="bg-success-subtle col-8 ">
+                <h1>QR Code Generator</h1>
+                <div className="col-11">
+                    <InputGroup className="mb-3 col-11 ">
                         <InputGroup.Text id="inputGroup-sizing-default">Name</InputGroup.Text>
                         <Form.Control
-                            aria-label="Đặt tên cho mã QR của bạn"
+
+                            className="d-flex"
+                            placeholder="Đặt tên cho mã QR của bạn"
                             aria-describedby="inputGroup-sizing-default"
                         />
                     </InputGroup>
                     <InputGroup className="mb-3">
                         <InputGroup.Text id="inputGroup-sizing-default">URL</InputGroup.Text>
                         <Form.Control
-                            aria-label="Đặt liên kết của bạn vào đây"
+                            onChange={(e) => setUrl(e.target.value)}
+                            placeholder="Đặt liên kết của bạn vào đây"
                             aria-describedby="inputGroup-sizing-default"
                         />
                     </InputGroup>
                 </div>
 
                 <div className="d-flex">
-                    <Button
-                        onClick={() => {setIsFrameComponent(true)}}
-                    >
+                    <Button onClick={handleMountStyleComponent}>
                         <FontAwesomeIcon icon={faGrip} />
                         <div>Phong Cách</div>
                     </Button>
-                    <Button
-                        onClick={() => setIsFrameComponent(true)}
-                    >
+                    <Button onClick={handleMountLogoComponent}>
                         <FontAwesomeIcon icon={faGrip} />
                         <div>Logo</div>
                     </Button>
-                    <Button
-                        onClick={() => setIsFrameComponent(true)}
-                    >
+                    <Button onClick={handleMountFrameComponent}>
                         <FontAwesomeIcon icon={faGrip} />
                         <div>Khung</div>
                     </Button>
@@ -61,7 +84,9 @@ function App() {
                 {isFrameComponent && <Frame />}
                 {isLogoComponent && <Logo />}
             </div>
-            <div className="bg-secondary-subtle col-4">sfsff</div>
+            <div className="bg-secondary-subtle col-4">
+                <QRcodeReview inputUrl={url} checkValid={handleValidUrlField}/>
+            </div>
         </div>
     );
 }
--- src/Component/QRcodeReview.js
+++ src/Component/QRcodeReview.js
@@ -1,10 +1,51 @@
+import { QRCode } from 'react-qrcode-logo';
+import { useEffect, useState } from 'react'
+import { Button } from "react-bootstrap"
+import { ToastContainer, toast } from 'react-toastify';
 
-const QRcodeReview = () => {
+import 'react-toastify/dist/ReactToastify.css';
+
+const QRcodeReview = ({inputUrl, checkValid}) => {
+    const [url, setUrl] = useState(window.location.href)
+
+    console.log(inputUrl)
+
+    const isUrlValid = (url) => {
+        try {
+          new URL(url);
+          return true;
+        } catch (error) {
+          return false;
+        }
+    };
+
+    const handleConvertLinkToQRcode = () => {
+        if(inputUrl === ""){
+            checkValid(false)
+            return toast.error("Trường này khong được để trống", {
+                position: "top-right",
+                autoClose: 5000,
+            });
+        } else if(!isUrlValid(isUrlValid)) {
+            checkValid(false)
+            return toast.error("URL không thỏa mãn, yêu cầu nhập lại URL khác, ", {
+                position: "top-right",
+                autoClose: 5000,
+            });
+        } else {
+            setUrl(inputUrl)
+        }
+    }
+
     return (
         <div className="QRcode-container">
             <h1>QRcodeReview</h1>
+            <QRCode value={url}/>
+            <Button onClick={handleConvertLinkToQRcode}>Xác nhận</Button>
+
+            <ToastContainer position="top-right" autoClose={5000}/>
         </div>
-    );
+    )
 };
 
 export default QRcodeReview;
